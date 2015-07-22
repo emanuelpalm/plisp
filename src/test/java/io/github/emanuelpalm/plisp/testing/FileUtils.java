ba7a0238e23e556f57811791d3deb42249da715d
Emanuel Palm <emanuelpalm@gmail.com>
2015-07-22 13:07:59 +0200

    Added testing.FileUtils#fromString(String).

---
+++ src/test/java/io/github/emanuelpalm/plisp/testing/FileUtils.java
@@ -0,0 +1,25 @@
+package io.github.emanuelpalm.plisp.testing;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+/**
+ * Various file utilities relevant for testing purposes.
+ */
+public class FileUtils {
+    /** Creates new temporary file containing given byte array. */
+    public static File fromBytes(final byte[] bs) throws IOException {
+        final File f = File.createTempFile("FileUtils_", ".tmp");
+        try (final FileOutputStream out = new FileOutputStream(f)) {
+            out.write(bs);
+        }
+        return f;
+    }
+
+    /** Creates new temporary file containing given string. */
+    public static File fromString(final String s) throws IOException {
+        return fromBytes(s.getBytes(StandardCharsets.UTF_8));
+    }
+}
