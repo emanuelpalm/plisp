01ec9cefd9d75b8b37ee1b583b5229f9e07283d9
Emanuel Palm <emanuelpalm@gmail.com>
2015-08-07 10:54:48 +0200

    Added more tests to parser.TestParser.

--- src/test/java/io/github/emanuelpalm/plisp/front/parser/TestParser.java
+++ src/test/java/io/github/emanuelpalm/plisp/front/parser/TestParser.java
@@ -5,6 +5,7 @@ import io.github.emanuelpalm.plisp.front.lexer.Lexer;
 import org.testng.annotations.Test;
 
 import java.util.Arrays;
+import java.util.Collections;
 
 import static io.github.emanuelpalm.util.testing.TreeUtils.*;
 import static org.testng.Assert.assertEquals;
@@ -18,5 +19,41 @@ public class TestParser {
 
     private Parser parserOf(final String s) {
         return new Parser(new BufferedLexer(Lexer.fromString(s)));
+    }
+
+    @Test
+    public void shouldParseList() {
+        final Tree t = parserOf("[1 2 3]").run();
+        assertEquals(t.root.nodes(), Collections.singletonList(listOf(integerOf("1"), integerOf("2"), integerOf("3"))));
+    }
+
+    @Test
+    public void shouldParseCall() {
+        final Tree t = parserOf("(fib 10)").run();
+        assertEquals(t.root.nodes(), Collections.singletonList(callOf(symbolOf("fib"), integerOf("10"))));
+    }
+
+    @Test
+    public void shouldParseMeta() {
+        final Tree t = parserOf("x: Integer").run();
+        assertEquals(t.root.nodes(), Collections.singletonList(metaOf(symbolOf("x"), symbolOf("Integer"))));
+    }
+
+    @Test
+    public void shouldParseComplexExpression() {
+        final Tree t = parserOf("(let fact [x: Integer]: Integer (foldl * 1 (range 1 x)))").run();
+        assertEquals(t.root.nodes(), Collections.singletonList(
+                callOf(
+                        symbolOf("let"),
+                        symbolOf("fact"),
+                        metaOf(listOf(metaOf(symbolOf("x"), symbolOf("Integer"))), symbolOf("Integer")),
+                        callOf(
+                                symbolOf("foldl"),
+                                symbolOf("*"),
+                                integerOf("1"),
+                                callOf(symbolOf("range"), integerOf("1"), symbolOf("x"))
+                        )
+                )
+        ));
     }
 }
