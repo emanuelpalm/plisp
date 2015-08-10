481fc9ab385c2f1854f3edada48b393753e5f34f
Emanuel Palm <emanuelpalm@gmail.com>
2015-08-10 20:23:40 +0200

    Updated parser.TreeSymbolTable.

--- src/main/java/io/github/emanuelpalm/plisp/front/parser/TreeSymbolTable.java
+++ src/main/java/io/github/emanuelpalm/plisp/front/parser/TreeSymbolTable.java
@@ -9,30 +9,30 @@ import java.util.Optional;
  * An abstract syntax {@link Tree} symbol table.
  */
 public class TreeSymbolTable {
-    private final HashMap<String, TreeNode> map;
-    private final BinaryTreeMap<String, TreeNode> tree;
+    private final HashMap<String, TreeNode> global;
+    private final BinaryTreeMap<String, TreeNode> local;
 
     /** Creates new empty symbol table. */
     public TreeSymbolTable() {
-        map = new HashMap<>();
-        tree = new BinaryTreeMap<>();
+        global = new HashMap<>();
+        local = new BinaryTreeMap<>();
     }
 
-    private TreeSymbolTable(final HashMap<String, TreeNode> map, final BinaryTreeMap<String, TreeNode> tree) {
-        this.map = map;
-        this.tree = tree;
+    private TreeSymbolTable(final HashMap<String, TreeNode> global, final BinaryTreeMap<String, TreeNode> local) {
+        this.global = global;
+        this.local = local;
     }
 
     /** Attempts to find symbol by given name. */
     public Optional<TreeNode> search(final String name) {
         return Optional.ofNullable(
-                tree.search(name).orElse(map.get(name))
+                local.search(name).orElse(global.get(name))
         );
     }
 
     /** Creates new symbol table also including given entry. */
     public TreeSymbolTable insertLocal(final String name, final TreeNode value) {
-        return new TreeSymbolTable(map, tree.insert(name, value));
+        return new TreeSymbolTable(global, local.insert(name, value));
     }
 
     /** Creates new symbol table also including given entry. */
