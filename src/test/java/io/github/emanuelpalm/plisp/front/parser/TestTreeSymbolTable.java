35000460f44df0e0cde669a490fc39d82e33b5bc
Emanuel Palm <emanuelpalm@gmail.com>
2015-08-10 20:34:52 +0200

    Updated TestTreeSymbolTable.

--- src/test/java/io/github/emanuelpalm/plisp/front/parser/TestTreeSymbolTable.java
+++ src/test/java/io/github/emanuelpalm/plisp/front/parser/TestTreeSymbolTable.java
@@ -1,11 +1,9 @@
 package io.github.emanuelpalm.plisp.front.parser;
 
-import io.github.emanuelpalm.plisp.front.lexer.Token;
-import io.github.emanuelpalm.plisp.front.lexer.TokenClass;
-import io.github.emanuelpalm.plisp.front.lexer.TokenOrigin;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
+import static io.github.emanuelpalm.util.testing.TreeUtils.*;
 import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertFalse;
 
@@ -14,48 +12,40 @@ public class TestTreeSymbolTable {
     public static Object[][] symbolTables() throws Throwable {
         return new Object[][]{
                 new Object[]{new TreeSymbolTable()
-                        .insertLocal(symNode("one"), intNode("10"))
-                        .insertLocal(symNode("two"), intNode("20"))
-                        .insertLocal(symNode("three"), intNode("30"))}
+                        .insertLocal(symbolOf("one"), integerOf("10"))
+                        .insertLocal(symbolOf("two"), integerOf("20"))
+                        .insertLocal(symbolOf("three"), integerOf("30"))}
         };
-    }
-
-    private static TreeNode.Symbol symNode(final String lexeme) {
-        return TreeNode.Symbol.of(new Token(new TokenOrigin("test", 1, 0), TokenClass.SYM, lexeme));
-    }
-
-    private static TreeNode.Integer intNode(final String lexeme) {
-        return TreeNode.Integer.of(new Token(new TokenOrigin("test", 1, 0), TokenClass.INT, lexeme));
     }
 
     @Test(dataProvider = "symbolTables")
     public void shouldFindValuesPreviouslyPut(final TreeSymbolTable t0) {
-        assertEquals(t0.search("one").get(), intNode("10"));
-        assertEquals(t0.search("two").get(), intNode("20"));
-        assertEquals(t0.search("three").get(), intNode("30"));
+        assertEquals(t0.search("one").get(), integerOf("10"));
+        assertEquals(t0.search("two").get(), integerOf("20"));
+        assertEquals(t0.search("three").get(), integerOf("30"));
     }
 
     @Test(dataProvider = "symbolTables")
     public void shouldReplaceExistingValueAssociatedWithSameKeyWhenUsingPut(final TreeSymbolTable t0) {
         final TreeSymbolTable t1 = t0
-                .insertLocal(symNode("one"), intNode("20"));
+                .insertLocal(symbolOf("one"), integerOf("20"));
 
-        assertEquals(t0.search("one").get(), intNode("10"));
-        assertEquals(t1.search("one").get(), intNode("20"));
+        assertEquals(t0.search("one").get(), integerOf("10"));
+        assertEquals(t1.search("one").get(), integerOf("20"));
     }
 
     @Test(dataProvider = "symbolTables")
     public void shouldCreateNewTablesWhenPuttingNewValues(final TreeSymbolTable t0) {
-        final TreeSymbolTable t1 = t0.insertLocal(symNode("four"), intNode("40"));
-        final TreeSymbolTable t2 = t1.insertLocal(symNode("five"), intNode("50"));
+        final TreeSymbolTable t1 = t0.insertLocal(symbolOf("four"), integerOf("40"));
+        final TreeSymbolTable t2 = t1.insertLocal(symbolOf("five"), integerOf("50"));
 
         assertFalse(t0.search("four").isPresent());
         assertFalse(t0.search("five").isPresent());
 
-        assertEquals(t1.search("four").get(), intNode("40"));
+        assertEquals(t1.search("four").get(), integerOf("40"));
         assertFalse(t1.search("five").isPresent());
 
-        assertEquals(t2.search("four").get(), intNode("40"));
-        assertEquals(t2.search("five").get(), intNode("50"));
+        assertEquals(t2.search("four").get(), integerOf("40"));
+        assertEquals(t2.search("five").get(), integerOf("50"));
     }
 }
