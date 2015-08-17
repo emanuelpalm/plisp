f876d5689a41301e5a18f4dd34afbd75c3d1ece1
Emanuel Palm <emanuelpalm@gmail.com>
2015-08-17 21:39:37 +0200

    Removed parser package.
    As the parser wasn't very feature complete, and I plan to make significant
    changes to parsing, I judged it easier to re-write it from scratch. This is a
    learning project after all, and practice makes perfect. ;)

--- src/main/java/io/github/emanuelpalm/plisp/Main.java
+++ src/main/java/io/github/emanuelpalm/plisp/Main.java
@@ -1,27 +1,10 @@
 package io.github.emanuelpalm.plisp;
 
-import io.github.emanuelpalm.plisp.front.lexer.BufferedLexer;
-import io.github.emanuelpalm.plisp.front.lexer.Lexer;
-import io.github.emanuelpalm.plisp.front.parser.Parser;
-import io.github.emanuelpalm.plisp.front.parser.TreeNode;
-
-import java.io.File;
-import java.io.IOException;
-
 /**
  * Application main class.
  */
 public class Main {
     public static void main(final String[] args) {
-        try {
-            final BufferedLexer l = new BufferedLexer(Lexer.fromFile(new File(args[0])));
-            final Parser p = new Parser(l);
-
-            final TreeNode t = p.run();
-            System.out.println(t);
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        System.out.println("plisp");
     }
 }
