3876046a2f8c1dbf5419b451a4a4b6c59997393f
Emanuel Palm <emanuelpalm@gmail.com>
2015-08-20 21:13:38 +0200

    Updated Main.

--- src/main/java/io/github/emanuelpalm/plisp/Main.java
+++ src/main/java/io/github/emanuelpalm/plisp/Main.java
@@ -1,10 +1,21 @@
 package io.github.emanuelpalm.plisp;
 
+import io.github.emanuelpalm.plisp.front.lexer.BufferedLexer;
+import io.github.emanuelpalm.plisp.front.lexer.Lexer;
+import io.github.emanuelpalm.plisp.front.parser.Parser;
+import io.github.emanuelpalm.plisp.front.parser.SExpr;
+
+import java.io.File;
+import java.io.IOException;
+
 /**
  * Application main class.
  */
 public class Main {
-    public static void main(final String[] args) {
-        System.out.println("plisp");
+    public static void main(final String[] args) throws IOException {
+        final BufferedLexer l = new BufferedLexer(Lexer.fromFile(new File(args[0])));
+        System.out.println(l);
+        final SExpr s = Parser.parse(l);
+        System.out.println(s);
     }
 }
