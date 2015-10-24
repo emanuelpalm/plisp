688ba012a09b4b7396e6cae964a67f78abf9e820
Emanuel Palm <emanuelpalm@gmail.com>
2015-10-24 16:00:32 +0200

    Update TestSExpr

--- src/test/java/io/github/emanuelpalm/plisp/runtime/TestSExpr.java
+++ src/test/java/io/github/emanuelpalm/plisp/runtime/TestSExpr.java
@@ -7,24 +7,24 @@ import static org.testng.Assert.assertEquals;
 public class TestSExpr {
     @Test
     public void shouldReturnNulIfCallingCarOrCdrOnAtom() {
-        final SExpr.Atom a = new SExpr.Atom("test");
+        final SExpr.Atom a = SExpr.Atom.of("test");
         assertEquals(a.car(), SExpr.NUL);
         assertEquals(a.cdr(), SExpr.NUL);
     }
 
     @Test
     public void shouldReturnGivenSExprsIfCallingCarOrCdrOnCons() {
-        final SExpr.Cons c = new SExpr.Cons(new SExpr.Atom("a"), new SExpr.Atom("b"));
-        assertEquals(c.car(), new SExpr.Atom("a"));
-        assertEquals(c.cdr(), new SExpr.Atom("b"));
+        final SExpr.Cons c = new SExpr.Cons(SExpr.Atom.of("a"), SExpr.Atom.of("b"));
+        assertEquals(c.car(), SExpr.Atom.of("a"));
+        assertEquals(c.cdr(), SExpr.Atom.of("b"));
     }
 
     @Test
     public void shouldConstructCorrectConsFromListOfSExprs() {
-        final SExpr s = SExpr.Cons.of(new SExpr.Atom("a"), new SExpr.Atom("b"), new SExpr.Atom("c"));
+        final SExpr s = SExpr.Cons.of(SExpr.Atom.of("a"), SExpr.Atom.of("b"), SExpr.Atom.of("c"));
         assertEquals(s, new SExpr.Cons(
-                new SExpr.Atom("a"), new SExpr.Cons(
-                new SExpr.Atom("b"), new SExpr.Cons(
-                new SExpr.Atom("c"), SExpr.NUL))));
+                SExpr.Atom.of("a"), new SExpr.Cons(
+                SExpr.Atom.of("b"), new SExpr.Cons(
+                SExpr.Atom.of("c"), SExpr.NUL))));
     }
 }
