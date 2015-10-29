c1ddcbfe97ea12a93f11e0c7d74b16e7c07ab04a
Emanuel Palm <emanuelpalm@gmail.com>
2015-10-29 19:15:15 +0100

    Simplify tests in TestParser

--- src/test/java/io/github/emanuelpalm/plisp/parser/TestParser.java
+++ src/test/java/io/github/emanuelpalm/plisp/parser/TestParser.java
@@ -1,42 +1,29 @@
 package io.github.emanuelpalm.plisp.parser;
 
-import io.github.emanuelpalm.plisp.lexer.BufferedLexer;
-import io.github.emanuelpalm.plisp.lexer.Token;
-import io.github.emanuelpalm.plisp.lexer.TokenClass;
 import io.github.emanuelpalm.plisp.runtime.SExpr;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
-import static io.github.emanuelpalm.util.testing.LexerUtils.lexerOf;
 import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.fail;
 
 public class TestParser {
     @Test(dataProvider = "lexers")
-    public void shouldParseIntoExpression(final BufferedLexer input, final SExpr expected) {
+    public void shouldParseIntoExpression(final String input, final SExpr expected) {
         assertEquals(Parser.parse(input), expected);
     }
 
     @DataProvider
     public Object[][] lexers() {
         return new Object[][]{
-                new Object[]{
-                        lexerOf(new Token(TokenClass.ATM, "x")),
-                        SExpr.Atom.of("x")
-                },
-                new Object[]{
-                        lexerOf(new Token(TokenClass.PAL, "("), new Token(TokenClass.ATM, "x"), new Token(TokenClass.PAR, ")")),
-                        SExpr.Cons.of(SExpr.Atom.of("x"))
-                },
-                new Object[]{
-                        lexerOf(new Token(TokenClass.QUO, "'"), new Token(TokenClass.ATM, "x")),
-                        SExpr.Cons.of(SExpr.Atom.of("quote"), SExpr.Atom.of("x"))
-                }
+                new Object[]{"x", SExpr.Atom.of("x")},
+                new Object[]{"(x)", SExpr.Cons.of(SExpr.Atom.of("x"))},
+                new Object[]{"'x", SExpr.Cons.of(SExpr.Atom.of("quote"), SExpr.Atom.of("x"))}
         };
     }
 
     @Test(dataProvider = "parserErrorLexers")
-    public void shouldFailWithParserError(final BufferedLexer input, final ParserError expected) {
+    public void shouldFailWithParserError(final String input, final ParserError expected) {
         try {
             Parser.parse(input);
             fail("No parser error generated.");
@@ -49,38 +36,14 @@ public class TestParser {
     @DataProvider
     public Object[][] parserErrorLexers() {
         return new Object[][]{
-                new Object[]{
-                        lexerOf(new Token(TokenClass.PAL, "(")),
-                        ParserError.UNBALANCED_PAL
-                },
-                new Object[]{
-                        lexerOf(new Token(TokenClass.PAR, ")")),
-                        ParserError.UNBALANCED_PAR
-                },
-                new Object[]{
-                        lexerOf(new Token(TokenClass.QUO, "'")),
-                        ParserError.DANGLING_QUOTE
-                },
-                new Object[]{
-                        lexerOf(new Token(TokenClass.PAL, "("), new Token(TokenClass.ATM, "x"), new Token(TokenClass.ATM, "y")),
-                        ParserError.UNBALANCED_PAL
-                },
-                new Object[]{
-                        lexerOf(new Token(TokenClass.PAL, "("), new Token(TokenClass.PAL, "("), new Token(TokenClass.PAR, ")")),
-                        ParserError.UNBALANCED_PAL
-                },
-                new Object[]{
-                        lexerOf(new Token(TokenClass.PAL, "("), new Token(TokenClass.PAR, ")"), new Token(TokenClass.PAR, ")")),
-                        ParserError.UNBALANCED_PAR
-                },
-                new Object[]{
-                        lexerOf(new Token(TokenClass.ATM, "x"), new Token(TokenClass.QUO, "'")),
-                        ParserError.DANGLING_QUOTE
-                },
-                new Object[]{
-                        lexerOf(new Token(TokenClass.PAL, "("), new Token(TokenClass.PAR, ")"), new Token(TokenClass.QUO, "'")),
-                        ParserError.DANGLING_QUOTE
-                },
+                new Object[]{"(", ParserError.UNBALANCED_PAL},
+                new Object[]{")", ParserError.UNBALANCED_PAR},
+                new Object[]{"'", ParserError.DANGLING_QUOTE},
+                new Object[]{"(x y", ParserError.UNBALANCED_PAL},
+                new Object[]{"(()", ParserError.UNBALANCED_PAL},
+                new Object[]{"())", ParserError.UNBALANCED_PAR},
+                new Object[]{"x'", ParserError.DANGLING_QUOTE},
+                new Object[]{"()'", ParserError.DANGLING_QUOTE},
         };
     }
 }
