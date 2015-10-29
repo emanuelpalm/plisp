ddf9d99fbe512ac106c1a45da2db942e72fee553
Emanuel Palm <emanuelpalm@gmail.com>
2015-10-29 21:15:33 +0100

    Make Main use runtime.Evaluator

--- src/main/java/io/github/emanuelpalm/plisp/Main.java
+++ src/main/java/io/github/emanuelpalm/plisp/Main.java
@@ -3,23 +3,44 @@ package io.github.emanuelpalm.plisp;
 import io.github.emanuelpalm.plisp.lexer.BufferedLexer;
 import io.github.emanuelpalm.plisp.lexer.Lexer;
 import io.github.emanuelpalm.plisp.parser.Parser;
+import io.github.emanuelpalm.plisp.runtime.Evaluator;
 import io.github.emanuelpalm.plisp.runtime.SExpr;
 
 import java.io.File;
-import java.io.IOException;
 
 /**
  * Application main class.
  */
 public class Main {
-    public static void main(final String[] args) throws IOException {
-        final BufferedLexer l = new BufferedLexer(Lexer.fromFile(new File(args[0])));
+    public static void main(final String[] args) {
+        if (args.length != 1) {
+            System.err.println("Usage: java -jar plisp.jar <file>");
+            return;
+        }
+
+        final BufferedLexer lexer;
         try {
-            final SExpr s = Parser.parse(l);
-            System.out.println(s);
+            lexer = new BufferedLexer(Lexer.fromFile(new File(args[0])));
+
+        } catch (final Throwable e) {
+            System.err.println("File error: " + e.getMessage());
+            return;
+        }
+
+        final SExpr expr;
+        try {
+            expr = Parser.parse(lexer);
 
         } catch (final Throwable e) {
             System.err.println("Parser error: " + e.getMessage());
+            return;
+        }
+
+        try {
+            System.out.println(Evaluator.eval(expr));
+
+        } catch (final Throwable e) {
+            System.err.println("Runtime error: " + e.getMessage());
         }
     }
 }
