e14cf16e6f7f90366a0f698cf5ceb3669dccefdb
Emanuel Palm <emanuelpalm@gmail.com>
2015-10-30 16:22:00 +0100

    Rename BufferedLexer to TokenBuffer

--- src/main/java/io/github/emanuelpalm/plisp/parser/Rule.java
+++ src/main/java/io/github/emanuelpalm/plisp/parser/Rule.java
@@ -1,6 +1,6 @@
 package io.github.emanuelpalm.plisp.parser;
 
-import io.github.emanuelpalm.plisp.lexer.BufferedLexer;
+import io.github.emanuelpalm.plisp.lexer.TokenBuffer;
 import io.github.emanuelpalm.plisp.lexer.Token;
 import io.github.emanuelpalm.plisp.lexer.TokenClass;
 import io.github.emanuelpalm.plisp.runtime.SExpr;
@@ -13,12 +13,12 @@ import java.util.Optional;
  */
 public interface Rule {
     /** Applies rule, returning {@link SExpr} only if successful. */
-    Optional<SExpr> apply(final BufferedLexer l);
+    Optional<SExpr> apply(final TokenBuffer b);
 
     /** Transforms rule into new rule using given transformer function. */
     default Rule transform(final Transformer t) {
-        return (l) -> {
-            final Optional<SExpr> s = apply(l);
+        return (buffer) -> {
+            final Optional<SExpr> s = apply(buffer);
             if (s.isPresent()) {
                 return Optional.of(t.onTransform(s.get()));
             }
@@ -35,44 +35,44 @@ public interface Rule {
 
     /** Requires one {@link Token} of given class. */
     static Rule oneOf(final TokenClass c) {
-        return (l) -> {
-            final int state = l.state();
-            final Token next = l.next();
+        return (buffer) -> {
+            final int state = buffer.state();
+            final Token next = buffer.next();
             if (next.type().equals(c)) {
                 return Optional.of(new SExpr.Atom(next));
             }
-            l.restore(state);
+            buffer.restore(state);
             return Optional.empty();
         };
     }
 
     /** Requires any one of the given rules to succeed. */
     static Rule anyOf(final Rule... rs) {
-        return (l) -> {
-            final int state = l.state();
+        return (buffer) -> {
+            final int state = buffer.state();
             for (final Rule r : rs) {
-                final Optional<SExpr> s = r.apply(l);
+                final Optional<SExpr> s = r.apply(buffer);
                 if (s.isPresent()) {
                     return Optional.of(s.get());
                 }
             }
-            l.restore(state);
+            buffer.restore(state);
             return Optional.empty();
         };
     }
 
     /** Requires all the the given rules to succeed. */
     static Rule allOf(final Rule... rs) {
-        return (l) -> {
-            final int state = l.state();
+        return (buffer) -> {
+            final int state = buffer.state();
             final ArrayList<SExpr> ss = new ArrayList<>(rs.length);
             for (final Rule r : rs) {
-                final Optional<SExpr> s = r.apply(l);
+                final Optional<SExpr> s = r.apply(buffer);
                 if (s.isPresent()) {
                     ss.add(s.get());
 
                 } else {
-                    l.restore(state);
+                    buffer.restore(state);
                     return Optional.empty();
                 }
             }
@@ -82,10 +82,10 @@ public interface Rule {
 
     /** Wraps all possible successful applications of the provided rule. Always succeeds. */
     static Rule manyOf(final Rule r) {
-        return (l) -> {
+        return (buffer) -> {
             final ArrayList<SExpr> ss = new ArrayList<>();
             while (true) {
-                final Optional<SExpr> s = r.apply(l);
+                final Optional<SExpr> s = r.apply(buffer);
                 if (s.isPresent()) {
                     ss.add(s.get());
 
